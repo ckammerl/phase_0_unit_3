dd71c503a85bcb5c2a1075987a078e426865791e
Christiane Kammerl <christiane@kammerl.com>
2014-04-24 11:15:56 -0700

    added final solution

--- week_7/7_gps3.1/our_solution.rb
+++ week_7/7_gps3.1/our_solution.rb
@@ -57,28 +57,34 @@ end
 
 #Refactored solution
 
-#DRAFT 
 
-module ShowItems #added module
-  def display(*item)
-    puts item.each {|item| puts item }
+class GroceryDisplay #added class
+  attr_reader :display
+
+    def initialize(grocery_list)
+      @grocery_list = grocery_list
+    end 
+
+  def display
+    puts "Your list contains: "
+    @grocery_list.items_list.each {|item| puts "- " + item}
   end
 end 
 
 
 class GroceryList 
-include ShowItems #seperated #display into module ShowItem
+  attr_reader :items_list #items_list now readable from outside class
 
   def initialize
     @items_list = []
   end 
-  
+
   def add_item(*item) #changed method name
     item.each { |elem| @items_list << elem }
   end 
   
   def count_items #changed method name
-   puts @items_list.length 
+   puts "#{@items_list.length} items on your list."
   end
   
   def check_off_item(item) #changed method name
@@ -87,19 +93,14 @@ include ShowItems #seperated #display into module ShowItem
         elem.prepend('x ')
       end 
     end
-      puts "Your updated grocery list: " 
-      display(@items_list)
     return @items_list
   end 
   
   def delete_item(item)
     @items_list.delete(item)
-    puts "Your updated grocery list: " 
-    display(@items_list) #added display after deletion
     return @items_list
   end  
 end 
-
 
 
 # DRIVER CODE GOES HERE. 
@@ -108,9 +109,18 @@ list = GroceryList.new
 list.add_item('Raisin Bran', 'Carrot', 'Beets')
 list.count_items
 list.check_off_item('Raisin Bran')
-list.delete_item('Raisin Bran')
+list.delete_item('Carrot')
+show_list = GroceryDisplay.new(list)
+puts ''
+show_list.display
 
-list.display(list)
+
+list2 = GroceryList.new
+list2.add_item('Tomato', 'Sugar', 'Honey')
+list2.check_off_item('Honey')
+show_list2 = GroceryDisplay.new(list2)
+puts ''
+show_list2.display
 
 #Reflection
 =begin
@@ -122,8 +132,7 @@ Did you learn any new skills or tricks?
 
 - STRING #prepend("string") 
 - separating unrelated functions/ methods; Emmanuel highlighted that #display is actually not a method that an instance of a class List 
-should include; After our team session, I thought about adding a second class that will include a method #display; but this second 
-class didn't have to create new instances, therefore I went back to read about modules and came up with the refactored solution.
+should include; After our team session, I added a second class that includes #display and came up with the refactored solution.
 
 
 How confident are you with each of the Learning Competencies? confident; but I'd like to practice writing classes/modules
